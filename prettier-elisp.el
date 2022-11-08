77eb038265f4e82671060e8210995a9e5437c112
KarimAziev <karim.aziiev@gmail.com>
2022-11-08 11:32:53 +0200

    update regex

--- prettier-elisp.el
+++ prettier-elisp.el
@@ -481,7 +481,7 @@ With ARG, do it that many times."
                                  (point))))))
           (save-excursion
             (save-match-data
-              (while (prettier-elisp-re-search-forward "[)][\n][\s]?+[)]" nil t
+              (while (prettier-elisp-re-search-forward "\\(\\]\\|)\\)[\n][\s]?+\\(\\]\\|)\\)" nil t
                                                        1)
                 (forward-char -1)
                 (join-line))))
